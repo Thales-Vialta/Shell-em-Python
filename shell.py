ea64ddeabb9793beffb1a677ccd64efb75dfcea8
Thales-Vialta <thalesvialta12@gmail.com>
2025-09-15 17:41:42 -0300

    0.2

--- shell.py
+++ shell.py
@@ -1,26 +1,36 @@
-import subprocess, os, math, shlex, sys
+import subprocess, os, shlex
+
 hist = []
+
+def executar_comando(cmd):
+    """Executa comando externo e adiciona ao histórico"""
+    try:
+        subprocess.run(shlex.split(cmd))
+        hist.append(cmd)
+    except FileNotFoundError:
+        print("Comando não encontrado:", cmd)
 
 while True: 
     try: 
         entrada = input(os.getcwd() + " psh> ")
-        if entrada.strip() == "":   
+        if not entrada.strip():   
             continue 
+        elif entrada in ["exit", "quit"]:
+            print("Saindo do shell...")
+            break
         elif entrada == "history": 
-            hist.append(entrada)   
             for i in range(len(hist)):
                 print(i, hist[i]) 
             continue
         elif entrada == "!!": 
-            if len(hist) == 0:       
+            if not hist:       
                 print("Sem registro ")
                 continue
             else: 
-                entrada = hist[-1]   
-                print("Executando: " + entrada)
-                subprocess.run(shlex.split(entrada))
-                hist.append(entrada)
-                continue
+                entrada = hist[-1]
+            print("Executando:", entrada)
+            executar_comando(entrada)
+            continue
         elif entrada.startswith("!"): 
             try: 
                 num_str = entrada[1:]       
@@ -46,7 +56,8 @@ while True:
                 os.chdir(new_dir)
             else: 
                 print("Diretório inválido")
-                continue
+            hist.append(entrada)
+            continue
         else: 
             subprocess.run(shlex.split(entrada))
             hist.append(entrada)
